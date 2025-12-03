320a9a955443f3037efeba303b548f00af24e230
Ivan227-hub <i.kushkov-de21@lll.kpi.ua>
2025-12-03 14:08:14 +0200

    add 03-react-movies project

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,5 +1,6 @@
-import axios, { AxiosResponse } from "axios";
-import { Movie } from "../types/movie";
+import axios from "axios";
+import type { AxiosResponse } from "axios";
+import type { Movie } from "../types/movie";
 
 const API_URL = "https://api.themoviedb.org/3";
 const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
