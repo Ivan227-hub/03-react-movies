09ab5504d130cd8c382d2fb3fd13c8acdec7f5f6
Ivan227-hub <i.kushkov-de21@lll.kpi.ua>
2025-12-08 12:33:36 +0200

    bugs fixed

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,9 +1,10 @@
 export interface Movie {
   id: number;
-  poster_path: string;
-  backdrop_path: string;
-  title: string;
-  overview: string;
-  release_date: string;
-  vote_average: number;
+  poster_path?: string | null;
+  backdrop_path?: string | null;
+  title?: string;
+  name?: string;
+  overview?: string | null;
+  release_date?: string;
+  vote_average?: number;
 }
